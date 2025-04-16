5feaa03d83cd812a9f56437995c18c8f0c12a541
EngOmarHamad <omar.mohammad.hamad2003@gmail.com>
2025-04-16 09:03:38 +0300

    first

--- app/src/main/java/com/example/smartmenuapp/models/User.java
+++ app/src/main/java/com/example/smartmenuapp/models/User.java
@@ -4,7 +4,7 @@ public class User {
     private String fullName;
     private String phone;
     private String email;
-
+    public User() {}
     public User(String fullName, String phone, String email) {
         this.fullName = fullName;
         this.phone = phone;
@@ -12,4 +12,13 @@ public class User {
     }
 
     // Getters and Setters
+
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
